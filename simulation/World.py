4677c4836d740c959115af78453a861e04873401
CommanderCero <dominikjeurissen@web.de>
2020-02-12 19:25:25 +0100

    fix raycast function

--- simulation/World.py
+++ simulation/World.py
@@ -1,17 +1,19 @@
 from .PolygonWall import PolygonWall
 import numpy as np
+import math
 
 class World:
     def __init__(self, walls, robot):
         self.walls = walls
         self.robot = robot
         
-    def raycast(self, start, direction):
-        end = start + direction
+    def raycast(self, start, direction, max_length):
+        norm_dir = direction / math.sqrt(direction[0] * direction[0] + direction[1] * direction[1])
+        end = start + norm_dir * max_length
         
         closest_inter = None
         closest_dist = max_length
-        for wall in walls:
+        for wall in self.walls:
             inter, dist = wall.check_line_intercept(start, end)
             
             # Check if the intersection is the closest to our start
