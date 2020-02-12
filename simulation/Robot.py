e7baaf2cb7d6375c49719e7c31727f1e4649c2f2
CommanderCero <dominikjeurissen@web.de>
2020-02-12 17:00:42 +0100

    add angle to robot

--- simulation/Robot.py
+++ simulation/Robot.py
@@ -4,6 +4,6 @@ class Robot:
     def __init__(self, start_x, start_y, radius=20):
         self.x = start_x
         self.y = start_y
-        
         self.radius = radius
         
+        self.angle = 0
