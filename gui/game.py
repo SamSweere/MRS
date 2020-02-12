8c2dcb006c39d60c5224ad089af939bd9d7fd84c
Sam Sweere <samsweere@gmail.com>
2020-02-12 17:08:17 +0100

    added environment parameters

--- gui/game.py
+++ gui/game.py
@@ -6,8 +6,6 @@ from simulation.World import World, create_rect_wall
 from simulation.Robot import Robot
 
 # Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 
 
@@ -16,19 +14,21 @@ class MyGame(arcade.Window):
     Main application class.
     """
 
-    def __init__(self):
+    def __init__(self, environment_width, environment_height):
+        self.SCREEN_WIDTH = environment_width
+        self.SCREEN_HEIGHT = environment_height
 
         # Call the parent class and set up the window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(self.SCREEN_WIDTH, self.SCREEN_HEIGHT, SCREEN_TITLE)
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
         walls = [
-            create_rect_wall(SCREEN_WIDTH/2 - 200 , SCREEN_HEIGHT/2 - 200, 100, 50)
+            create_rect_wall(self.SCREEN_WIDTH/2 - 200 , self.SCREEN_HEIGHT/2 - 200, 100, 50)
         ]
-        robot = Robot(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        robot = Robot(self.SCREEN_WIDTH/2, self.SCREEN_HEIGHT/2)
         self.world = World(walls, robot)
 
     def on_draw(self):
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from gui.game import MyGame
 import arcade
 
-if __name__ == "__main__":
-    window = MyGame()
+environment_params = {
+        "environment_width" : 1000,
+        "environment_height": 650
+    }    
+
+if __name__ == "__main__":    
+    window = MyGame(**environment_params)
     window.setup()
     arcade.run()
