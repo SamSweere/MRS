9900d8e45523e0fec55282fbfdfa2058ab5334c3
Sam Sweere <samsweere@gmail.com>
2020-03-14 23:22:48 +0100

    added arrowkeys

--- gui/human_controller.py
+++ gui/human_controller.py
@@ -31,26 +31,26 @@ class HumanController:
                         self.robot.update_vl(0)
             elif self.scenario == "localization":
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_w or event.key == pygame.K_UP:
                         # Set a big number, will be limited by max v
                         self.robot.update_v(1000)
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                         self.robot.update_v(-1000)
-                    if event.key == pygame.K_a:
+                    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                         self.robot.rotate_left = True
                         # self.robot.update_angle(-1)
-                    if event.key == pygame.K_d:
+                    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                         self.robot.rotate_right = True
                         # self.robot.update_angle(1)
                 if event.type == pygame.KEYUP:
                     # Stop
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_w or event.key == pygame.K_UP:
                         self.robot.update_v(0)
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                         self.robot.update_v(0)
-                    if event.key == pygame.K_a:
+                    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                         self.robot.rotate_left = False
                         # self.robot.update_angle(0)
-                    if event.key == pygame.K_d:
+                    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                         self.robot.rotate_right = False
                         # self.robot.update_angle(0)
